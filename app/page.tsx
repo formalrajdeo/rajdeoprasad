fe50ef70cde8fa5e57aacf03f2444247afd94c64
raj <formal.rajdeo@gmail.com>
2024-03-05 00:06:09 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,232 @@
-import Image from "next/image";
+import { FaFacebookSquare, FaTwitterSquare, FaYoutube, FaGithub } from "react-icons/fa";
+import { IoDocument } from "react-icons/io5";
+import { IoIosContact, IoMdMail } from "react-icons/io";
+import Link from "next/link";
+import { PERSONAL_DATA } from '../constants/PERSONAL_DATA'
 
-export default function Home() {
+const HEADER = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <header className="flex justify-between items-center">
+      <div className="">
+        <div className="">
+          <span className="font-semibold text-3xl">RAJDEO PRASAD</span>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className=""><span>DEVELOPER</span></div>
+          <div className="px-2 mx-2 border-t-0 border-b-0 border-l border-r border-black">
+            <span>YOUTUBER</span>
+          </div>
+          <div className=""><span>TEACHER</span></div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex justify-between items-center">
+        <div className="bg-black text-white p-2 m-1">
+          <Link href="#">
+            <FaFacebookSquare />
+          </Link>
+        </div>
+        <div className="bg-black text-white p-2 m-1">
+          <Link href="#">
+            <FaTwitterSquare />
+          </Link>
+        </div>
+        <div className="bg-black text-white p-2 m-1">
+          <Link href="#">
+            <FaYoutube />
+          </Link>
+        </div>
+        <div className="bg-black text-white p-2 m-1">
+          <Link href="#">
+            <FaGithub />
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
+}
+const NAVIGATION = () => {
+  return (
+    <div className="my-4">
+      <ul className="flex justify-center items-center gap-4 bg-gray-200">
+        <li className="flex flex-col px-8 py-4 justify-center items-center hover:bg-gray-300 hover:cursor-pointer">
+          <div className="">
+            <IoIosContact size={50} color="gray" />
+          </div>
+          <div className="">
+            <Link href="#">Profile</Link>
+          </div>
+        </li>
+        <li className="flex flex-col px-8 py-4 justify-center items-center">
+          <div className="">
+            <IoDocument size={50} color="gray" />
+          </div>
+          <div className="">
+            <Link href="#">Resume</Link>
+          </div>
+        </li>
+        <li className="flex flex-col px-8 py-4 justify-center items-center">
+          <div className="">
+            <IoMdMail size={50} color="gray" />
+          </div>
+          <div className="">
+            <Link href="#">Contact</Link>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
+const EMPLOYEE = () => {
+  return (
+    <div className="flex flex-col justify-center pr-8 mt-4">
+      <div className="border-t-0 border-l-0 border-r-0 border-b border-black py-2">
+        <span className="font-semibold">Employment</span>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {PERSONAL_DATA.EMPLOYEE.map((emp, emp_idx) => {
+        return (
+          <div key={emp_idx} className="mt-2 border-t-0 border-r-0 border-b-0 border-l border-black pl-2">
+            <div className="flex justify-between items-center text-sm py-2">
+              <div className="">
+                <div className="">
+                  <span>{emp.company_name}</span>
+                </div>
+                <div className="">
+                  <span className="text-gray-500">{emp.designation}</span>
+                </div>
+              </div>
+              <div className="">
+                <span className="text-blue-700">{emp.from_date} - {emp.to_date}</span>
+              </div>
+            </div>
+            <p className="text-xs">
+              {emp.role_description}
+            </p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+const EDUCATION = () => {
+  return (
+    <div className="flex flex-col justify-center pr-8 mt-4">
+      <div className="border-t-0 border-l-0 border-r-0 border-b border-black py-2">
+        <span className="font-semibold">Education</span>
       </div>
-    </main>
+
+      {PERSONAL_DATA.EDUCATION.map((edu, edu_idx) => {
+        return (
+          <div key={edu_idx} className="mt-2 border-t-0 border-r-0 border-b-0 border-l border-black pl-2">
+            <div className="flex justify-between items-center text-sm py-2">
+              <div className="">
+                <div className="">
+                  <span>{edu.degree}</span>
+                </div>
+                <div className="">
+                  <span className="text-gray-500">{edu.college_name}</span>
+                </div>
+              </div>
+              <div className="">
+                <span className="text-blue-700">{edu.from_date} - {edu.to_date}</span>
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+const PROGRAMMING_LANGUAGE = () => {
+  return (
+    <div className="flex flex-col justify-center mb-4">
+      <div className="border-t-0 border-l-0 border-r-0 border-b border-black py-2">
+        <span className="font-semibold">{PERSONAL_DATA.PROGRAMMING_LANGUAGES.title}</span>
+      </div>
+      <div className="mt-2 flex flex-col">
+        {PERSONAL_DATA.PROGRAMMING_LANGUAGES.value.map((prog_lang, prog_lang_idx) => {
+          return (
+            <div key={prog_lang_idx} className="flex justify-between items-center text-sm py-2">
+              <div className="">
+                <span>{prog_lang.name}</span>
+              </div>
+              <div className="flex justify-between items-center gap-1">
+                {[...Array(prog_lang.rating)].map((_, index) => (
+                  <div key={index} className="w-4 h-4 rounded-full bg-gray-500"></div>
+                ))}
+                {[...Array(7 - prog_lang.rating)].map((_, index) => (
+                  <div key={index} className="w-4 h-4 rounded-full bg-gray-300"></div>
+                ))}
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+const SKILLS = () => {
+  return (
+    <>
+      {PERSONAL_DATA.SKILLS.map((skill, skill_idx) => {
+        return (
+          <div key={skill_idx} className="flex flex-col justify-center mb-4">
+            <div className="border-t-0 border-l-0 border-r-0 border-b border-black py-2">
+              <span className="font-semibold">{skill.title}</span>
+            </div>
+            {skill.value.map((skill_data, skill_data_idx) => {
+              return (
+                <div key={skill_data_idx} className="mt-2">
+                  <div className="flex justify-between items-center text-sm py-2">
+                    <div className="">
+                      <span>{skill_data.name}</span>
+                    </div>
+                    <div className="flex justify-between items-center gap-1">
+                      {[...Array(skill_data.rating)].map((_, index) => (
+                        <div key={index} className="w-4 h-4 rounded-full bg-gray-500"></div>
+                      ))}
+                      {[...Array(7 - skill_data.rating)].map((_, index) => (
+                        <div key={index} className="w-4 h-4 rounded-full bg-gray-300"></div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+const FOOTER = () => {
+  return (
+    <footer className="bg-slate-600 mt-4 w-full h-6">
+    </footer>
+  )
+}
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center flex-col pt-12">
+      <div className="w-11/12">
+        <HEADER />
+        <NAVIGATION />
+        <div className="flex justify-between items-center">
+          <div className="w-3/4">
+            <EMPLOYEE />
+            <EDUCATION />
+          </div>
+          <div className="w-3/12">
+            <PROGRAMMING_LANGUAGE />
+            <SKILLS />
+          </div>
+        </div>
+      </div>
+      <FOOTER />
+    </div>
   );
 }
