c90b503442f4953c27fde804a0cf50d3b246e112
raj <formal.rajdeo@gmail.com>
2024-03-05 14:18:14 +0530

    config fixed

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       </div>
       <div
         className="my-8 py-8 px-8 mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-        <Image className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" priority={true} src={ProfileImg} alt="Woman's Face" width={100} height={300} />
+        <Image className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" priority={true} src={ProfileImg} alt="" width={100} height={300} />
         <div className="text-center space-y-2 sm:text-left">
           <div className="space-y-0.5">
             <p className="text-lg text-black font-semibold">
