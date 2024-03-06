ab248543ca296c7082ae463eda266c351b892f3c
raj <formal.rajdeo@gmail.com>
2024-03-06 11:04:22 +0530

    icon center

--- containers/resume/Navigation.tsx
+++ containers/resume/Navigation.tsx
@@ -27,15 +27,15 @@ const Navigation = () => {
     const pathname = usePathname()
     return (
         <div className="my-4">
-            <ul className="flex justify-center items-center gap-4 bg-gray-200">
+            <ul className="flex justify-center items-center gap-4 shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-4">
                 {NAV_LINKS.map((link, link_indx) => {
                     return (
-                        <Link key={link_indx} href={link.href} className={`px-5 py-3 flex flex-col justify-center items-center ${pathname === link.href ? 'bg-gray-400 text-white' : 'hover:bg-gray-300 hover:cursor-pointer hover:text-gray-700'}`}>
+                        <Link key={link_indx} href={link.href} className={`rounded-lg px-5 py-3 flex flex-col justify-center items-center ${pathname === link.href ? 'bg-gray-400 text-white' : 'hover:bg-gray-300 hover:cursor-pointer hover:text-gray-700'}`}>
                             <li>
-                                <div>
+                                <div className='flex justify-center items-center'>
                                     {link.icon}
                                 </div>
-                                <div>
+                                <div className='flex justify-center items-center text-center'>
                                     {link.title}
                                 </div>
                             </li>
